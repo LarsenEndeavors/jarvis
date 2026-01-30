207f6382b3e6e99a65a2d920273e0828f4a7e949
LarsenEndeavors <larsenendeavors@gmail.com>
2026-01-29 20:20:18 -0600

    epic/0/dod: Added skeletal files and updated routes.ts

--- jarvis/portfolio/app/routes.ts
+++ jarvis/portfolio/app/routes.ts
@@ -1,3 +1,26 @@
-import { type RouteConfig, index } from "@react-router/dev/routes";
+import { type RouteConfig, index, prefix, route } from "@react-router/dev/routes";
 
-export default [index("routes/home.tsx")] satisfies RouteConfig;
+export default [
+    index("routes/home.tsx"),
+    
+    ...prefix("work", [
+        index("./routes/work_index.tsx"),
+        route(":slug", "./routes/work.$slug.tsx"),
+    ]),
+
+    ...prefix("stories", [
+        index("./routes/stories_index.tsx"),
+        route(":slug", "./routes/stories.$slug.tsx"),
+    ]),
+
+    ...prefix("blog", [
+        index("./routes/blog_index.tsx"),
+        route(":slug", "./routes/blog.$slug.tsx"),
+    ]),
+
+    route("lab", "./routes/lab.tsx"),
+    route("contact","./routes/contact.tsx"),
+
+    // Catchall
+    route("*", "./routes/$.tsx")
+] satisfies RouteConfig;
