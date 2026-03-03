521ebdfaa34773bf1fee367c87c33b779d49ef29
fen1x <larsenendeavor@gmail.com>
2026-03-02 20:51:05 -0600

    shop: Installed Snipcart

--- jarvis/portfolio/app/root.tsx
+++ jarvis/portfolio/app/root.tsx
@@ -63,6 +63,8 @@ export default function Root() {
         </div>
         <ScrollRestoration />
         <Scripts />
+        <script src="/scripts/snipcart-init.js" defer></script>
+        <div hidden id="snipcart" />
       </body>
     </html>
   );
